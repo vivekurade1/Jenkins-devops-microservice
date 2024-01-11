5b83540888f203560eeb6f9ed3a6f30ea2a28a4f
Vivek Urade <vivekurade@gmail.com>
2024-01-11 17:33:14 +0530

    hhhh

--- src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
+++ src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -1,16 +1,23 @@
 package com.in28minutes.microservices.currencyexchangeservice;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.in28minutes.microservices.currencyexchangeservice.resource.CurrencyExchangeController;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CurrencyExchangeServiceApplicationTests {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Test
 	public void contextLoads() {
+
+		LOGGER.info("test file is executing");
 	}
 
 }
